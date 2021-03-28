34c4356d17c00741ecf895700e15bc3ab1b684a8
Nicholas Prieto <nicholasmprieto@gmail.com>
2021-03-28 07:46:16 -0400

    improved solution for Triangle problem in Exercism javascript track

--- exercism/javascript/triangle/triangle.js
+++ exercism/javascript/triangle/triangle.js
@@ -9,14 +9,19 @@ export class Triangle {
   }
 
   get isEquilateral() {
-    return this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && sides.every((side) => { return side !== 0 });
+    return this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && this.sides.every((side) => side !== 0) && this.sides.reduce((acc, curr) => acc + curr) - Math.max(...this.sides) >= Math.max(...this.sides);
   }
 
   get isIsosceles() {
-    throw new Error('Remove this statement and implement this function');
+    return (this.sides[0] === this.sides[1] || this.sides[1] === this.sides[2] || this.sides[0] === this.sides[2]) && this.sides.every((side) => side !== 0) && this.sides.reduce((acc, curr) => acc + curr) - Math.max(...this.sides) >= Math.max(...this.sides);
   }
 
   get isScalene() {
-    throw new Error('Remove this statement and implement this function');
+    return this.sides[0] !== this.sides[1] && this.sides[1] !== this.sides[2] && this.sides[0] !== this.sides[2] && this.sides.every((side) => side !== 0) && this.sides.reduce((acc, curr) => acc + curr) - Math.max(...this.sides) >= Math.max(...this.sides);
   }
 }
+
+// const triangle = new Triangle(7, 3, 2);
+// console.log(triangle.sides.reduce((acc, cur) => acc + cur));
+// console.log(triangle.sides.reduce((acc, cur) => acc + cur) - Math.max(...triangle.sides));
+// console.log(triangle.sides.reduce((acc, cur) => acc + cur) - Math.max(...triangle.sides) >= Math.max(...triangle.sides));
